719c13be7f295265f7a0b3431c69dfddb4399667
Ssebi <sebidancau@yahoo.com>
2020-08-28 09:44:07 +0300

    navbar,banner,about

--- coding/app.js
+++ coding/app.js
@@ -3,7 +3,16 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+//EJS setup
+const ejs = require('ejs');
+app.set('view engine', ejs);
+
 //Connect to the server on port 8080 by default
 app.listen(port, (error) => {
 	if (error) throw error;
+});
+
+//Routes
+app.get('/', (req, res) => {
+	res.render('index.ejs');
 });
