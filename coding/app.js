8337439fdeed7cb16417dca9189da23bca7457d1
Ssebi <sebidancau@yahoo.com>
2020-09-01 10:07:22 +0300

    login,register

--- coding/app.js
+++ coding/app.js
@@ -3,19 +3,54 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+//Env setup
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+//Database connect
+const mysql = require('mysql');
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+});
+
+db.connect((err) => {
+    if (err) throw err;
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 //EJS setup
 const ejs = require('ejs');
-app.set('view engine', ejs);
+app.set('view engine', 'ejs');
 
 //Connect to the server on port 8080 by default
 app.listen(port, (error) => {
-	if (error) throw error;
+    if (error) throw error;
 });
 
 //Root css file
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 //Routes
 app.get('/', (req, res) => {
-	res.render('index.ejs');
+    res.render('index.ejs');
 });
+
+app.get('/login', (req, res) => {
+    if (!req.cookies.username) res.render('login.ejs');
+    else res.redirect('/');
+});
+
+app.get('/register', (req, res) => {
+    if (!req.cookies.username) res.render('register.ejs');
+    else res.redirect('/');
+});
+
+app.use('/auth', require('./routes/auth.js'));
